0956e884026d48e1d829d5e596b7dbd85469d72d
Saicharan4688 <166994265+Saicharan4688@users.noreply.github.com>
2025-02-22 20:50:09 +0530

    Update main.tf

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "demo-terraform-eks-state-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
